4c7840bda833f16c61dcb1dc44c141cbb3df057c
hfchan0 <1155143743@link.cuhk.edu.hk>
2022-03-25 15:44:17 +0800

    update on Admin

--- SystemUser/Admin.java
+++ SystemUser/Admin.java
@@ -1,7 +1,8 @@
 
 package SystemUser;
 import java.sql.Connection;
-
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner; // class for user input
 import java.util.Set;
 public class Admin {
@@ -14,7 +15,7 @@ public class Admin {
     public Scanner input_scanner = new Scanner(System.in);
     private String operation; 
     public Admin(Connection connection){
-        con = connection;
+        this.con = connection;
     }
     public void operation(){
         Boolean isUsing = true;
@@ -70,19 +71,125 @@ public class Admin {
         System.out.print("Enter Yours Choice: ");
     }
     private void CreateTableSchemas(){
-
-    }
+        System.out.print("Processing...");
+        //initialize Statement
+        try{
+            Statement stmt=con.createStatement();
+            //SQL Query
+            //userCategory
+            String userCategoryQ="CREATE TABLE userCategory(" +
+                    "ucid ," +
+                    "max," +
+                    "period," + 
+                    "PRIMARY KEY (sid, cid)," +
+                    "FOREIGN KEY (sid) REFERENCES " +
+                    ")";	     
+                stmt.executeUpdate(userCategoryQ);
+            String userQ="CREATE TABLE user(" +
+                "uid, " +
+                "name, " +
+                "age, " + 
+                "occupation, " +
+                "ucid," +
+                "PRIMARY KEY ()," + 
+                "FOREIGN KEY () REFERENCES " +
+                ")";	     
+                stmt.executeUpdate(userQ);
+            String carCategoryQ="CREATE TABLE carCategory(" +
+                "ccid ," +
+                "ccname," +
+                "PRIMARY KEY ()," + 
+                "FOREIGN KEY () REFERENCES " +
+                ")";	     
+                stmt.executeUpdate(carCategoryQ);
+            String carQ="CREATE TABLE car(" +
+                "callnum, " +
+                "name, " +
+                "manufacture, " + 
+                "time_rent, " +
+                "ccid, " +
+                "PRIMARY KEY(), " +
+                "FOREIGN KEY () REFERENCES " +
+                ")";	     
+                stmt.executeUpdate(carQ);
+            String copyQ="CREATE TABLE copy(" +
+                "callnum," +
+                "copynum," +
+                "PRIMARY KEY()" +
+                ")";	     
+                stmt.executeUpdate(copyQ);
+            String rentQ="CREATE TABLE rent(" +
+                "uid, " +
+                "callnum, " +
+                "copynum, " +
+                "checkout, " +
+                "return," +
+                "PRIMARY KEY ()," + 
+                "FOREIGN KEY () REFERENCES " +
+                ")";	     
+                stmt.executeUpdate(rentQ);
+            String produceQ="CREATE TABLE produce(" +
+                "cname ," +
+                "callnum," +
+                "PRICE varchar(10)," + 
+                "PRIMARY KEY ()," + 
+                "FOREIGN KEY () REFERENCES " +
+                ")";	     
+                stmt.executeUpdate(produceQ);
+        }
+        catch (SQLException e){
+            System.out.println(e);
+        }
+        System.out.println("Done. Database is initialized.");
+    } 
+    
 
     private void DeleteTableSchemas(){
-
+        System.out.printf("Processing...");
+        try{
+            //initialize Statement
+            Statement stmt=con.createStatement();
+            //SQL Query
+            String deleteTableQuery="DROP TABLE IF EXISTS userCategory, " +
+                                    "user, " +
+                                    "carCategory, " +
+                                    "car, " +
+                                    "copy, " +
+                                    "rent, " +
+                                    "produce" ;
+            
+            //Run Query
+            stmt.executeUpdate(deleteTableQuery);
+        }
+        catch (SQLException e){
+            System.out.println(e);
+        } 
+        System.out.println("Done. Database is removed.");
+        
     }
 
     private void LoadData(){
+        System.out.println();
+        System.out.print("Type in the Source Data Folder Path: ");
+        String path;
+        Scanner pathReader = new Scanner(System.in);
+        path = pathReader.nextLine();
 
+        System.out.println("Done. Data is inputted to the database.");
+        pathReader.close();
     }
 
     private void ShowRecordNumber(){
-
+        System.out.println("Number of records in each table:");
+        System.out.println();
     }
 
 }
+
+// userCategory (ucid, max, period)
+// user (uid, name, age, occupation, ucid)
+// car_category (ccid, ccname)
+// car (callnum, name, manufacture, time_rent, ccid)
+// copy (callnum, copynum)
+// rent (uid, callnum, copynum, checkout, return)
+// produce (cname, callnum)
